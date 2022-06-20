c6c4cf73984aca3728dc6896292f5307ad017ebe
asdfghjkl268 <dimavladimirov7032@gmail.com>
2022-06-20 20:37:24 +0200

    Update statgen.py

--- src/engine/statgen.py
+++ src/engine/statgen.py
@@ -8,3 +8,12 @@ def generate_list():
 			t.append(random.randint(1, 6))
 		a.append(sum(t)-min(t))
 	return a
+
+
+def stats(order):
+	a = generate_list() 
+	a.sort(reverse = True)
+	t = [0,0,0,0,0,0]
+	for i in range(0, 6):
+		t[order[i]]=a[i]
+	return t
