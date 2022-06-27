b370a8bbf69655ce44928865278727c2a9d14468
asdfghjkl268 <dimavladimirov7032@gmail.com>
2022-06-27 20:24:49 +0200

    //

--- src/engine/datacheck.py
+++ src/engine/datacheck.py
@@ -1,15 +1,15 @@
 def check_back():
-	flag = True
 	for i in range(1, 35):
 		with open('resources/backstorytables/'+str(i)+'.txt') as f:
 			arr = f.readlines()
 		for j in arr:
 			tarr=arr[j].split(".")
 			if tarr[len(tarr)-1]=="" or tarr[len(tarr)-1]=="\n":
-				flag= True
+				return True
 			else:
 				try:
 					t = int(tarr[len(tarr)-1])
 				except:
-					flag = False
-	return flag
+					return "table:"+str(i)+" line:"+str(j)
+				finally:
+					return True
